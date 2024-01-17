74b5266a37f0416aced98f9fd472df848149b773
CesarOrtiz1994 <cesarortize94@gmail.com>
2024-01-17 17:30:33 -0600

    promesa con error

--- Promesas/promesas.js
+++ Promesas/promesas.js
@@ -1,19 +1,10 @@
-//uso de promesas
-// las promesas son objetos que representan la terminacion o el fracaso eventual de una operacion asincrona
-
-let promesaExitosa = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("La operacion fue exitosa");
-    }, 3000);
+let promesaConError = new Promise((resolve, reject) => {
+    reject('Algo salió mal');
 });
 
-
-//manejo de promesas
-//then() se ejecuta cuando la promesa se resuelve exitosamente
-//catch() se ejecuta cuando la promesa se rechaza
-
-promesaExitosa.then((mensaje) => {
+//Manejar la promesa con error
+promesaConError.then((mensaje) => {
     console.log(mensaje);
 }).catch((error) => {
-    console.error(error);
+    console.log(error);
 });

551aefe55cea021617275f83a8cb040c18865312
CesarOrtiz1994 <cesarortize94@gmail.com>
2024-01-17 17:23:26 -0600

    uso de promesas

---
+++ Promesas/promesas.js
@@ -0,0 +1,19 @@
+//uso de promesas
+// las promesas son objetos que representan la terminacion o el fracaso eventual de una operacion asincrona
+
+let promesaExitosa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("La operacion fue exitosa");
+    }, 3000);
+});
+
+
+//manejo de promesas
+//then() se ejecuta cuando la promesa se resuelve exitosamente
+//catch() se ejecuta cuando la promesa se rechaza
+
+promesaExitosa.then((mensaje) => {
+    console.log(mensaje);
+}).catch((error) => {
+    console.error(error);
+});
